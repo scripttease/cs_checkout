0aee535fb0ccf4219f0bdb3515b3570c43705096
Al Dee <amyalicedee@gmail.com>
2023-03-29 10:16:20 +0100

    Adds bogof offer

--- checkout.rb
+++ checkout.rb
@@ -1,8 +1,9 @@
 class Checkout
+
   ITEMS = [
-    { product_code: 'FR1', name: 'Fruit tea', price: 3.11 },
-    { product_code: 'SR1', name: 'Strawberries', price: 5.00 },
-    { product_code: 'CF1', name: 'Coffee', price: 11.23},
+    { product_code: 'FR1', name: 'Fruit tea', price: 3.11, offer: 'bogof', offer_applied: false },
+    { product_code: 'SR1', name: 'Strawberries', price: 5.00,  offer: 'three_or_more', offer_applied: false },
+    { product_code: 'CF1', name: 'Coffee', price: 11.23, offer: 'none', offer_applied: false }
   ]
 
   attr_reader :basket
@@ -18,7 +19,37 @@ class Checkout
     self
   end
 
-  def total
-    @basket.map { |i| i[:price] }.sum
+  def check_for_and_apply_bogof_offer
+    bogof_product = @basket.find { |item|
+      item[:offer] == 'bogof' && item[:offer_applied] == false
+    }
+    return self if bogof_product.nil?
+
+    count_two_or_more_products = @basket.select { |item|
+      item[:offer] == 'bogof' && item[:offer_applied] == false
+    }
+    return self if count_two_or_more_products.count < 2
+
+    bogof_product_code = bogof_product[:product_code]
+
+    @basket
+      .select { |item| item[:product_code] == bogof_product_code }
+      .reject { |item| item[:offer_applied] }
+      .each.with_index { |item, index|
+        item[:price] = 0 if index.odd? && index > 0
+        item[:offer_applied] = true
+      }
+
+    check_for_and_apply_bogof_offer
+      
+    self
   end
+
+
+  def total
+    check_for_and_apply_bogof_offer
+    
+    total = @basket.map { |item| item[:price] }.sum || 0
+  end
+
 end
--- checkout_test.rb
+++ checkout_test.rb
@@ -5,10 +5,29 @@ require_relative 'checkout'
 class CheckoutTest < Minitest::Test
 
   def test_scan_item
-    assert_equal [{ product_code: 'FR1', name: 'Fruit tea', price: 3.11, }] , Checkout.new.scan('FR1').basket
+    assert_equal [{ product_code: 'FR1', name: 'Fruit tea', price: 3.11, offer: 'bogof', offer_applied: false},] , Checkout.new.scan('FR1').basket
+  end
+
+  def test_scan_incorrect_product_code
+    assert_equal [], Checkout.new.scan('AB1').basket
+  end
+
+  def test_scan_no_product_code
+    assert_equal [], Checkout.new.scan('').basket
   end
 
   def test_scan_one_item_checkout_total
     assert_equal 3.11, Checkout.new.scan('FR1').total
+  end
+
+  def test_scan_two_items_checkout_total
+    assert_equal 8.11, Checkout.new.scan('FR1').scan('SR1').total
+  end
+
+  def test_bogof_offer_applied_to_two_items
+    assert_equal 3.11, Checkout.new([
+      { product_code: 'FR1', name: 'Fruit tea', price: 3.11, offer: 'bogof', offer_applied: false },
+      { product_code: 'FR1', name: 'Fruit tea', price: 3.11, offer: 'bogof', offer_applied: false },
+    ]).total
   end
 end
