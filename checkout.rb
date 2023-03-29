9ab8e64ce3df141c6b7e05426a18a28698162d82
Al Dee <amyalicedee@gmail.com>
2023-03-29 09:24:54 +0100

    Checkout scans and calculates total

--- checkout.rb
+++ checkout.rb
@@ -1,2 +1,24 @@
 class Checkout
+  ITEMS = [
+    { product_code: 'FR1', name: 'Fruit tea', price: 3.11 },
+    { product_code: 'SR1', name: 'Strawberries', price: 5.00 },
+    { product_code: 'CF1', name: 'Coffee', price: 11.23},
+  ]
+
+  attr_reader :basket
+
+  def initialize(basket = [])
+    @basket = basket
+  end
+
+  def scan(item_product_code = '')
+    item = ITEMS.find { |i| i[:product_code] == item_product_code }
+    
+    @basket << item unless item.nil?
+    self
+  end
+
+  def total
+    @basket.map { |i| i[:price] }.sum
+  end
 end
--- checkout_test.rb
+++ checkout_test.rb
@@ -1,8 +1,14 @@
 require 'minitest/autorun'
 require_relative 'checkout'
 
+
 class CheckoutTest < Minitest::Test
 
   def test_scan_item
+    assert_equal [{ product_code: 'FR1', name: 'Fruit tea', price: 3.11, }] , Checkout.new.scan('FR1').basket
+  end
+
+  def test_scan_one_item_checkout_total
+    assert_equal 3.11, Checkout.new.scan('FR1').total
   end
 end
